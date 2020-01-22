8e318533c9642f2f2666b7c2b13e01d533455afc
root <root@k8s-m.srv.local>
2020-01-22 16:13:18 +0300

    Ср янв 22 16:13:18 MSK 2020

--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 
 ### first create secret from passwd.py
 ```# kubectl create secret generic tbot-passwd.py --from-file=./passwd.py```
+
+### create haproxy ConfigMap for telegram proxy
+```# kubectl create configmap tbot-haproxy-config --from-file ./haproxy/haproxy.cfg```
--- passwd.example.py
+++ passwd.example.py
@@ -4,8 +4,7 @@ ipamuser='username'
 ipampass='*****'
 admins = ["123456789","123456780"]
 proxy = {
-        'https':'socks5h://user:****@www.ru:8443',
-        'http':'socks5://www2.ru:80',
+        'https':'socks5h://user:****@tbot-proxy:443'
         }
 TOKEN='337991232135:AAFbIhrwHwc4gOV2-68X6F3O4y8123123123';
 url = 'www....'
