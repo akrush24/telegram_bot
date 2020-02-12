adbe9308bc807ac0b1f615e318820383c74248b4
Andrey <akrush24@gmail.com>
2020-02-12 22:08:49 +0300

    Delete check.py

--- check.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-from telebot import apihelper, TeleBot
-#import config
-from passwd import TOKEN, admins, proxy
-
-apihelper.proxy = proxy
-bot = TeleBot(TOKEN)
-chat_id=admins[0]
-bot.send_message(chat_id, 'Wake up!')
